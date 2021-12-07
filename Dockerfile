4b88fb8cf5471593d6fe55c53f2eafb04e22b528
Dmitriy Rusakov <rusd80@gmail.com>
2021-12-07 16:52:26 +0300

    fix dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,30 @@
-FROM python:3.8-slim AS compiler
-WORKDIR /app
-COPY /app /app 
-RUN apt-get update
-RUN apt-get install -y upx patchelf binutils libgcc1
+FROM python:3.6-slim AS compiler
 
-RUN pip3 install pyinstaller virtualenv staticx
-RUN pip freeze > requirements.txt
+WORKDIR /app
+COPY /app /app
+
+RUN apt-get update \
+&& apt-get install -y patchelf binutils libgcc1 upx\
+&& pip3 install pyinstaller virtualenv staticx \
+&& pip install --upgrade pip
+
 RUN python -m venv venv
 ENV PATH $PATH:venv/bin/activate
 RUN . venv/bin/activate
-
-RUN pip3 install Flask
 RUN pip3 install -r /app/requirements.txt
-RUN PYTHONOPTIMIZE=1 pyinstaller --onefile --strip -F --add-data "countries.json:countries.json" --add-data "languages.json:languages.json" \
-			--paths /venv/lib/site-packages \
-			-a --clean --upx-dir=/usr/bin/ app.py
 
-RUN staticx -l /lib/x86_64-linux-gnu/libgcc_s.so.1 --strip /app/dist/app appc
+
+RUN PYTHONOPTIMIZE=1 pyinstaller --onefile --strip --paths /venv/lib/site-packages \
+			-a --clean --upx-dir=/usr/bin/ app.py \
+&& staticx -l /lib/x86_64-linux-gnu/libgcc_s.so.1 --strip /app/dist/app appc
+
+RUN mkdir /app/tmp
 
 FROM scratch AS tiny
-WORKDIR /
-COPY --from=compiler /tmp /tmp
+
+COPY --from=compiler /app/tmp /tmp
 COPY --from=compiler /app/appc .
 COPY --from=compiler /app/countries.json .
 COPY --from=compiler /app/languages.json .
 EXPOSE 8080
-CMD ["/appc"]  
+CMD ["/appc"]
--- app.py
+++ app.py
@@ -1,7 +1,35 @@
 from flask import Flask, json, request
 from json import load
 
+
+# function returns info about country
+def get_info(query: str) -> str:
+    with open('countries.json', 'r', encoding='utf-8') as file:
+        countries = load(file)
+    with open('languages.json', 'r', encoding='utf-8') as file:
+        languages = load(file)
+    if len(query) == 2 and query.isalpha():
+        try:
+            result = "country: {0}, \ncapital: {1}, \ncurrency: {2}, \nlanguages(native): "\
+                .format(countries[query]["name"], countries[query]["capital"], countries[query]["currency"],)
+            native = []
+            for lang in countries[query]["languages"]:
+                native.append("{0}({1})".format(languages[lang]["name"], languages[lang]["native"]))
+            result += ", ".join(native)
+            result += "\n"
+        except KeyError:
+            result = "country not found"
+    else:
+        result = "data incorrect"
+    return result
+
+
 app = Flask(__name__)
+
+
+@app.route("/country/<path:sub>")
+def get_sub(sub):
+    return get_info(sub.upper())
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -10,24 +38,7 @@ def get_query():
         query = json.loads(request.get_data())["CountryCode"].upper()
     except Exception:
         return "data incorrect"
-    with open('countries.json', 'r', encoding='utf-8') as file:
-        countries = load(file)
-    with open('languages.json', 'r', encoding='utf-8') as file:
-        languages = load(file)
-
-    if len(query) == 2 and query.isalpha():
-        try:
-            result = "Country: {0}, capital: {1}, currency: {2}, languages(native): "\
-                .format(countries[query]["name"], countries[query]["capital"], countries[query]["currency"],)
-            native = []
-            for lang in countries[query]["languages"]:
-                native.append("{0}({1})".format(languages[lang]["name"], languages[lang]["native"]))
-            result += ", ".join(native)
-        except KeyError:
-            result = "country not found"
-    else:
-        result = "data incorrect"
-    return result
+    return get_info(query)
 
 
 if __name__ == "__main__":
